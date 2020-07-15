5ab81d0af42856a3e427b7b94141dae5015b15af
Henrik Johansson <henrik.johansson712@gmail.com>
2020-07-15 15:27:46 +0200

    Add documentation to scicat-service

--- common/scicat-service.js
+++ common/scicat-service.js
@@ -5,6 +5,12 @@ const superagent = require('superagent');
 const baseUrl = process.env.BASE_URL || 'http://localhost:3030/api/v3';
 
 exports.Dataset = class {
+  /**
+   * Query SciCat datasets matching filter
+   * @param {object} filter SciCat loopback filter object
+   * @returns {object[]} Array of SciCat dataset objects
+   */
+
   async find(filter) {
     try {
       const jsonFilter = JSON.stringify(filter);
@@ -18,6 +24,13 @@ exports.Dataset = class {
       throw err;
     }
   }
+
+  /**
+   * Find a SciCat dataset by id
+   * @param {string} id SciCat dataset pid
+   * @param {object} filter SciCat loopback filter object
+   * @returns {object} SciCat dataset object
+   */
 
   async findById(id, filter) {
     try {
@@ -35,6 +48,12 @@ exports.Dataset = class {
     }
   }
 
+  /**
+   * Get count of SciCat datasets matching filter
+   * @param {object} filter SciCat loopback filter object
+   * @returns {object} Object with key `count` and number of matching SciCat datasets as value
+   */
+
   async count(filter) {
     try {
       const jsonFilter = JSON.stringify(filter);
@@ -49,6 +68,13 @@ exports.Dataset = class {
       throw err;
     }
   }
+
+  /**
+   * Get origDatablocks for SciCat dataset
+   * @param {string} id SciCat dataset pid
+   * @param {object} filter SciCat loopback filter object
+   * @returns {object[]} Array of SciCat origDatablock objects
+   */
 
   async findByIdFiles(id, filter) {
     try {
@@ -68,6 +94,12 @@ exports.Dataset = class {
 };
 
 exports.PublishedData = class {
+  /**
+   * Query SciCat publications matching filter
+   * @param {object} filter SciCat loopback filter object
+   * @returns {object[]} Array of SciCat publishedData objects
+   */
+
   async find(filter) {
     try {
       const jsonFilter = JSON.stringify(filter);
@@ -81,6 +113,13 @@ exports.PublishedData = class {
       throw err;
     }
   }
+
+  /**
+   * Find a SciCat publication by id
+   * @param {string} id SciCat publishedData doi
+   * @param {object} filter SciCat loopback filter object
+   * @returns {object} SciCat publishedData object
+   */
 
   async findById(id, filter) {
     try {
@@ -98,6 +137,12 @@ exports.PublishedData = class {
     }
   }
 
+  /**
+   * Get count of SciCat publications mathing query
+   * @param {object} where SciCat where filter object
+   * @returns {object} Object with key `count` and number of matching SciCat publications as value
+   */
+
   async count(where) {
     try {
       const jsonWhere = JSON.stringify(where);
@@ -114,6 +159,12 @@ exports.PublishedData = class {
 };
 
 exports.Instrument = class {
+  /**
+   * Query SciCat instruments matching query
+   * @param {object} filter SciCat loopback filter object
+   * @returns {object[]} Array of SciCat instrument objects
+   */
+
   async find(filter) {
     try {
       const jsonFilter = JSON.stringify(filter);
@@ -127,6 +178,13 @@ exports.Instrument = class {
       throw err;
     }
   }
+
+  /**
+   * Find a SciCat instrument by id
+   * @param {string} id SciCat instrument id
+   * @param {object} filter SciCat loopback filter object
+   * @returns {object} SciCat instrument object
+   */
 
   async findById(id, filter) {
     try {
@@ -144,6 +202,12 @@ exports.Instrument = class {
     }
   }
 
+  /**
+   * Get count of SciCat instruments matching query
+   * @param {object} where SciCat where filter object
+   * @returns {object} Object with key `count` and number of matching SciCat instruments as value
+   */
+
   async count(where) {
     try {
       const jsonWhere = JSON.stringify(where);
@@ -160,6 +224,12 @@ exports.Instrument = class {
 };
 
 exports.Sample = class {
+  /**
+   * Query SciCat samples matching query
+   * @param {object} filter SciCat loopback filter object
+   * @returns {object[]} Array of SciCat sample objects
+   */
+
   async find(filter) {
     try {
       const jsonFilter = JSON.stringify(filter);
